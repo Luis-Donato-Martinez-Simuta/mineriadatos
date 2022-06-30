f896a89aba7b49427fab8cf8c8a0ac2a9a3c7258
203405 <203405@ids.upchiapas.edu.mx>
2022-06-30 13:12:22 -0500

    first commit

---
+++ src/main/java/com/api/apidemo/Controller.java
@@ -0,0 +1,13 @@
+package com.api.apidemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/status")
+    public String status() {
+        return "{\n    \"status\":\"200\",\n    \"mensage_global\":\"okey\"\n}";
+    }
+
+}
