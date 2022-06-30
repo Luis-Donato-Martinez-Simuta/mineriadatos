d88e78718372f6bef7efea7599ba09785b402b0f
203405 <203405@ids.upchiapas.edu.mx>
2022-06-30 14:43:58 -0500

    6 comit

--- src/main/java/com/api/apidemo/ApidemoApplication.java
+++ src/main/java/com/api/apidemo/ApidemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApidemoApplication {
 
 	public static void main(String[] args) {
+
+		
 		SpringApplication.run(ApidemoApplication.class, args);
 	}
 
